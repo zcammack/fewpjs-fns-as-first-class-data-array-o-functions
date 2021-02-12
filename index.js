896e662183a12f97215c3a8842a0f38567121c60
zcammack <zack.cammack@gmail.com>
2021-02-12 07:48:56 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+function wakeDog(dogName, dogBreed) {
+    let string = `Wake ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function leashDog(dogName, dogBreed) {
+    let string = `Leash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function walkToPark(dogName, dogBreed) {
+    let string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+function walkHome(dogName, dogBreed) {
+    let string = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string;
+}
+
+function unleashDog(dogName, dogBreed) {
+    let string = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    return routine.map(fn => fn(dogName, dogBreed))
+}
