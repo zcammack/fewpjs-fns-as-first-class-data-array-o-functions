f3dc6d82fd356c87e118dfe6375183b89721db61
zcammack <zack.cammack@gmail.com>
2021-02-12 07:51:24 -0600

    commit tree b300c8569b62e26d6570484e58eebdededcfad49

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ function unleashDog(dogName, dogBreed) {
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-    return routine.forEach(fn => fn(dogName, dogBreed))
+    return routine.map(fn => fn(dogName, dogBreed))
 }
